1852ab73710b98f88ab75a8e1ffd798780d52646
David Jepsson <davjep-0@student.ltu.se>
2022-02-09 14:21:43 +0100

    Amening file, adding .NET support

--- Program.cs
+++ Program.cs
@@ -1,19 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace questionMaster
-{
-    public class Program 
-    {
-        static void Main(string[] args) {
-            string message = "Hello World";
+namespace questionMaster {
+    public class Program {
+        public static void Main(string[] args) {
+            string hello = "Hello World";
 
-            Console.WriteLine(message);
-
+            Console.WriteLine(hello);
         }
-        
     }
 }
